0e260c24bf162a6fe1236a9b9c89d60a223a4103
kushal bhargava <kushal.git01@gmail.com>
2024-04-14 17:25:48 +0530

    initial commit

---
+++ Array/Class.js
@@ -0,0 +1,21 @@
+class Player {
+  constructor(name, type) {
+    this.name = name;
+    this.type = type;
+  }
+  introduce() {
+    console.log(`Hi I am ${this.name}, I'm a ${this.type}`);
+  }
+}
+
+class Wizard extends Player {
+  constructor(name, type) {
+    super(name, type);
+  }
+  play() {
+    console.log(`Weeee I'm a ${this.type}`);
+  }
+}
+
+const wizrd1 = new Wizard("Rohit", "Attacker");
+const wizrd2 = new Wizard("Raj", "Poisner");
